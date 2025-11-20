ce6668b6b8532348df67a54dbbda5435e615265e
Vinifut10 <vinitxuco17@gmail.com>
2025-11-20 01:01:29 -0300

    Criar toggleTheme.js

---
+++ toggleTheme.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("toggleTheme");
+
+// Se tiver tema salvo no localStorage, aplica
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    // Salva preferência
+    if (document.body.classList.contains("dark")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
